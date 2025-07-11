73d3f3986493a82c2d074ad35035e56edf90c921
Ajay6433 <Ajaykhan6433@gmail.com>
2025-07-11 21:05:43 +0530

    added checkout page

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -3,13 +3,18 @@ import React from 'react'
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import Spinner from '../components/Spinner';
+import { useDispatch } from 'react-redux';
+import { addItem } from '../store/cartSlice';
+import toast from 'react-hot-toast';
 
 function ProductDetails() {
   const { id } = useParams();
   const [product, setProduct] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const dispatch = useDispatch();
 
+  
   useEffect(() => {
     const fetchProduct = async () => {
       try {
@@ -26,22 +31,27 @@ function ProductDetails() {
         setLoading(false);
       }
     };
-
+    
     fetchProduct();
   }, [id]);
 
   if (loading) {
     return <Spinner />;
   }
-
+  
   if (error) {
     return <div className="text-red-500 text-center mt-4">Error: {error.message}</div>;
   }
-
+  
   if (!product) {
     return <div className="text-center mt-4">Product not found.</div>;
   }
-
+  function addToCart(product){
+    toast.success(`${product.title} is added to the cart`);
+    console.log("Product added to cart:", product);
+    dispatch(addItem(product));
+  }
+  
   return (
     <div className="container mx-auto p-4">
       <div className="flex flex-col md:flex-row items-center">
@@ -53,7 +63,9 @@ function ProductDetails() {
           <p className="text-gray-700 text-xl mb-4">${product.price}</p>
           <p className="text-gray-600 mb-4">{product.description}</p>
           <p className="text-gray-500 text-sm">Category: {product.category}</p>
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4">
+          <button 
+          onClick={()=> addToCart(product)}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4">
             Add to Cart
           </button>
         </div>
